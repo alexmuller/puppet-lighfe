ec70dec1a144dc6fb58cea4882ae545a1e9400e1
Alex Muller <alex@mullr.net>
2013-10-05 13:46:41 +0100

    Add alexmuller user and groups

--- Puppetfile
+++ Puppetfile
@@ -1,3 +1,5 @@
 forge 'http://forge.puppetlabs.com/'
 
 mod 'puppetlabs/stdlib', '~> 3.0'
+
+mod 'torrancew/account', '0.0.5'
--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,8 @@
 # Use hiera as a lightweight ENC.
 node default {
   hiera_include('classes')
+
+  # Set up users and groups
+  $accounts = hiera('accounts')
+  create_resources('account', $accounts)
 }
